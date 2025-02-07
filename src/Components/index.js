191613b4622297e41657d94c1b608a1c5f070105
Krishnakandgole <krashnakandagole@gmail.com>
2025-02-07 14:37:01 +0530

    file chnage

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header/Header";
 import Profile from "./Profile/Profile";
 import Message from "./Message/Message";
-import SavedPost from "./Profile/SavedPost";
+// import SavedPost from "./Profile/SavedPost";
 import Login from "./Login/Login";
 import Signup from "./Login/Signup";
 export {
